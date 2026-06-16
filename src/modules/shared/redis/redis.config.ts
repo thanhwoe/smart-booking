b407368657af4b51be222d998a897efeeb441bfd
thanh.nguyen-dev <70120251+thanhwoe@users.noreply.github.com>
2026-06-16 11:32:36 +0700

    fix:Redis authentication (#7)
    Co-authored-by: thanh.nguyentrung <thanh.nguyentrung@asnet.com.vn>

--- src/modules/shared/redis/redis.config.ts
+++ src/modules/shared/redis/redis.config.ts
@@ -7,8 +7,6 @@ export const redisFactory = (config: ConfigService) => {
   return new Redis({
     host: config.getOrThrow<string>('REDIS_HOST'),
     port: config.getOrThrow<number>('REDIS_PORT'),
-
-    //   TODO: production
     password: config.get<string>('REDIS_PASSWORD'),
     //   tls: configService.get<boolean>('REDIS_TLS') ? {} : undefined,
     maxRetriesPerRequest: null,
